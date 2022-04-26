118d1bbd14c8ba1c89b7412373c511a15595e147
Nikita <nukuta228336@gmail.com>
2022-04-26 22:41:02 +0200

    cycles

---
+++ Learning_js/ cycles.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let num = 55;
+
+while (num >= 23) {
+    console.log(num);
+    num--;
+}
+
+let num2 = 20;
+
+do {
+    console.log(num2);
+    num2++;
+}
+while(num2 <= 25);
+
+let num3 = 15;
+
+for(let i = 1; i < 5; i++) {
+    console.log(num3);
+    num3++;
+}
