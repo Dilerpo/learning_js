3973b0580ddc519a747e2cf020750d5bbe7d9092
Nikita <nukuta228336@gmail.com>
2022-04-27 17:33:19 +0200

    cycles2

--- Learning_js/ cycles.js
+++ Learning_js/ cycles.js
@@ -21,3 +21,69 @@ for(let i = 1; i < 5; i++) {
     console.log(num3);
     num3++;
 }
+
+
+for(let i = 2; i <= 10; i++) {
+    if(i === 3) {
+        continue;
+    }
+    if(i === 5) {
+        continue;
+    }
+    if(i === 7) {
+        continue;
+    }
+    if(i === 9) {
+        continue;
+    }
+    console.log(i);
+}
+
+
+for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+
+
+let i = 2;
+
+while(i < 16) {
+    i++;
+    if (i % 2 === 0) {
+        continue;
+    }else {
+        console.log(i);
+    }
+}
+
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+
+    for (let i = 5; i < 11; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+
+    console.log(arrayOfNumbers);
+    return arrayOfNumbers;
+}
+
+
+
+let result = "";
+const lengt = 7;
+
+for (let i = 1; i < lengt; i++) {
+
+    for (let j = 0; j < i; j++) {
+        result += "*";
+    }
+
+    result += "\n";
+}
+
+console.log(result);
