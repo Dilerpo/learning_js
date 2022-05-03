de37ca71d9e132a2f4a96c9f3e722a12f4b367be
Nikita <nukuta228336@gmail.com>
2022-05-03 12:07:45 +0200

    methods & properties

---
+++ methods & properties.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+const fruit = "Some fruiet";
+
+console.log(fruit.indexOf("i"));
+
+const logg = ("Hello World");
+
+console.log(logg.slice(5, 11));
+
+console.log(logg.substr(5, 3));
+
+const number = (14.6);
+
+console.log(Math.round(number));
+
+const test = ("14.4px");
+
+console.log(parseInt(test));
+
+console.log(parseFloat(number));
